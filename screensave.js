38022ea507a29489c2cfbee3e2e090081cdfc2b1
Kai Hendry <hendry@webconverger.com>
2012-02-20 14:41:27 +0800

    first commit

---
+++ screensave.js
@@ -0,0 +1,19 @@
+var screenTimer;
+document.onmousemove = resetTimer;
+
+window.onload = function() {
+	screenTimer = setTimeout(inactive, 2000);
+};
+
+function inactive() {
+	// screen saver goes here
+	document.body.style.display = "none";
+}
+
+function resetTimer(e) {
+	// undo screen saver here
+	document.body.style.display = "block";
+	// reset timer
+	clearTimeout(screenTimer);
+	screenTimer = setTimeout(inactive, 2000);
+}
